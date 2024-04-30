ab673ea46eb1d1ab6c0d53e428b94cd8c54714a3
stringsplit <77242831+nbitzz@users.noreply.github.com>
2024-04-29 17:02:42 -0700

    Abort abort abort

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,22 +1,4 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Topbar from "./elem/Topbar.svelte";
-    import PulldownManager from "./elem/PulldownManager.svelte";
-    import UploadWindow from "./elem/UploadWindow.svelte";
-    import { pulldownManager } from "./elem/stores.js";
-    
-    let topbar: Topbar;
-
-    let pulldown: PulldownManager;
-
-    onMount(() => {
-        pulldownManager.set(pulldown)
-    })
 </script>
 
-<Topbar bind:this={topbar} pulldown={pulldown} />
-<div id="appContent">
-    <PulldownManager bind:this={pulldown} />
-
-    <UploadWindow/>
-</div>
+<div id="appContent"></div>
